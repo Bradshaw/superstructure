948da6fc5dffe9827e35f9e08fc4f20e7d247cb5
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-05-13 15:59:28 +0200

    feat: adds markdown to article compilation with pug templates

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const sass = require('node-sass');
+const pug = require('pug');
+const md = require('markdown-it')({
+	html: true,
+	breaks: true,
+});
 
 const cwd =  process.cwd();
 
@@ -51,7 +56,7 @@ async function compileCss(config){
 		const targetdir = path.parse(target).dir;
 		fs.mkdir(targetdir, { recursive: true })
 			.then(()=>{
-				console.log(`Compile ${file} to ${target}`);
+				// console.log(`Compile ${file} to ${target}`);
 				sass.render({
 					file: file,
 					options: {
@@ -63,15 +68,71 @@ async function compileCss(config){
 				});
 			});
 	}
+}
 
+async function compileHtml(config, templates){
+	const htmlPath = path.join(config.root, config.markdown);
+	const files = await walk(htmlPath);
+
+	for (const file of files){
+		const parsed = path.parse(file);
+		const target = path.join(config.dest, file.replace(htmlPath, "").replace(parsed.ext, '.html'));
+		const targetdir = path.parse(target).dir;
+		fs.mkdir(targetdir, { recursive: true })
+			.then(async ()=>{
+				console.log(`Compile ${file} to ${target}`);
+				const [markdown, yaml] = (await fs.readFile(file, {encoding: "utf-8"})).split(/(?=%YAML)/);
+				console.log(yaml);
+				const article = md.render(markdown);
+				const html = templates.layout({
+					title: "A page",
+					content: article
+				})
+				fs.writeFile(target, html);
+			});
+	}
+}
+
+async function getTemplates(config){
+	const templatePath = path.join(config.root, config.templates);
+	const files = await walk(templatePath);
+	let templates = {};
+	for (const file of files){
+		const templateFilename = path.parse(file).name
+		templates[templateFilename] = pug.compile(await fs.readFile(file), {
+			basedir: templatePath,
+			filename: templateFilename
+		});
+	}
+	return templates;
+}
+
+
+var arraything = [
+	{
+		name: "thing",
+		data: "The data"
+	},
+	{
+		name: "otherthing",
+		data: "More data"
+	},
+]
+
+var things = {
+	thing: "The data",
+	otherthing: "More data"
 }
 
 
 let superstructure = {
 	build: async (config)=>{
 		try{
+			const templates = (await getTemplates(config));
+			console.log(templates);
 			await copyPublic(config);
 			await compileCss(config);
+			await compileHtml(config, templates);
 		} catch (err) {
 			console.error(err);
 		}
